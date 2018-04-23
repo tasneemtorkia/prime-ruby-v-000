6e579a5c22cfefe4c6d3fc884d46960c88a75ff7
tasneem torkia <tasneemtorkia@hotmail.co.uk>
2018-04-23 13:23:38 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,10 @@
 # Add  code here!
 def prime(num)
-(2..integer - 1).each do |x|
-  if (integer % x) == 0
-   return false
-  else
-   return true
-  end
-end
+ return false if integer < 2
+    (2..integer - 1).each do |x|
+        if (integer % x) == 0
+            return false
+        end
+    end
+    true
 end
